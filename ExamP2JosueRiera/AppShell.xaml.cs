1fe1ba22d58c5ff66415db7312ab8d41319fdcd5
josue riera <josueriera23@hotmail.com>
2024-11-29 07:13:54 -0500

    Add project files.

---
+++ ExamP2JosueRiera/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamP2JosueRiera
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExamP2JosueRiera/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamP2JosueRiera
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
